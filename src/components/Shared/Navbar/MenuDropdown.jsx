0ac827059df2b494f629f34c4b1b1a5fc5bc6a2b
anUnnecessary <raihanshamil33@gmail.com>
2023-06-21 12:36:45 +0600

    category added

--- src/components/Shared/Navbar/MenuDropdown.jsx
+++ src/components/Shared/Navbar/MenuDropdown.jsx
@@ -15,7 +15,7 @@ const MenuDropdown = () => {
       <div className='flex flex-row items-center gap-3'>
         {/* Aircnc btn */}
         <div className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'>
-          AirCNC your home
+            AirCNC Home
         </div>
         {/* Dropdown btn */}
         <div
