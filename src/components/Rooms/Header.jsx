35858eed9d5526c6f61ac53bd3e397d65542cb19
anUnnecessary <raihanshamil33@gmail.com>
2023-06-22 15:32:19 +0600

    data info added

--- src/components/Rooms/Header.jsx
+++ src/components/Rooms/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <>
       <Heading
-        title='Veluvana Bali - Owl Bamboo House'
-        subtitle='Sidemen, Indonesia'
+        title={roomData.title}
+        subtitle={roomData.location}
       />
       <div className='w-full md:h-[60vh] overflow-hidden rounded-xl'>
         <img
           className='object-cover w-full'
-          src='https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg'
+          src={roomData.image}
           alt='header image'
         />
       </div>
